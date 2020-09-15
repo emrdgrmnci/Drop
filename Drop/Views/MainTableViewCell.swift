7f3cb7164adceec9b30efb508fdc2fe59c101cff
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-09-15 18:34:12 +0300

    Add main table view cell elements

---
+++ Drop/Views/MainTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MainTableViewCell.swift
+//  Drop
+//
+//  Created by Emre Değirmenci on 15.09.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var todoTitleLabel: UILabel!
+    @IBOutlet weak var todoSwitch: UISwitch!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
