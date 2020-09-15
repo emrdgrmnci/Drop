b5ee88007e4d33ce6774cf212c4411b6b37fcebe
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-09-15 18:08:36 +0300

    Setup initializers of interactor and presenter

--- Drop/Scenes/Main/MainViewController.swift
+++ Drop/Scenes/Main/MainViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-    
+protocol MainDisplayLogic: class {
+    func displayTodayData(viewModel: ListToday.FetchToday.ViewModel)
+}
+
+class MainViewController: UIViewController, MainDisplayLogic {
+
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
-    
+
+    var interactor: MainBusinessLogic?
+
+    var displayedData: [Todo] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+
+    // MARK: Setup
+    private func setup() {
+        let viewController = self
+        let interactor = MainInteractor()
+        let presenter = MainPresenter()
+        viewController.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+    }
+
+    func displayTodayData(viewModel: ListToday.FetchToday.ViewModel) {
+
     }
 }
